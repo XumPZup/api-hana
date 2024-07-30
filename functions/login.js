1caef72ce8bc7261336d27596a78eaf988344a0c
XumPZuP <linomarco65@gmail.com>
2024-07-30 05:51:04 +0000

    SSL

--- functions/login.js
+++ functions/login.js
@@ -17,6 +17,7 @@ exports.handler = async function(event, context) {
       serverNode: data.server + ":" + data.port,
       UID: data.uid,
       PWD: data.pwd,
+      sslValidateCertificate: 'false'
     };
 
     const connectToHana = () => {
